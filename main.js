1b14b9e13eba2befec6e949c9f8e69bd0d24f941
Sean Hong <writeseanhong@gmail.com>
2016-09-16 18:57:13 -0700

    finished first page of design added media query and also link scroll

--- main.js
+++ main.js
@@ -9,14 +9,17 @@ $(document).ready(function(){
         down = e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0;
         c = Math.min(Math.max(0, down ? ++c : --c), tot-1);
         pagePos = $pages.eq(c).offset().top - 65;
-
         $(this).stop().animate({scrollTop: pagePos}, 650, function(){
             listen = true;
         });
-
     });
-    // $(document.body).on("DOMMouseScroll mousewheel", function(e){
-    //     console.log('this is the event: ', e);
-    // })
+    $('.navbar-nav a').on('click', function(){
+       console.log('this is what was clicked on: ', this.getAttribute('class'));
+        var eq_value = this.getAttribute('class');
+        var dom_to_scroll_to = $('.pages').eq(eq_value).offset().top - 65;
+        var current_position = dom_to_scroll_to;
+        $('body').animate({scrollTop: dom_to_scroll_to}, 650);
+    })
 });
+
 
