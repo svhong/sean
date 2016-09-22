58c0f1586f18a59b54b71008dad4ccf2f1f1040a
Sean Hong <writeseanhong@gmail.com>
2016-09-22 14:51:28 -0700

    added applications section with title and working links

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     var $pages = $(".pages"),
         tot = $pages.length,
-        c = 0, pagePos = 0, down = 0, listen = true;
-    $('html, body').on('DOMMouseScroll mousewheel', function(e) {
+        current = 0, pagePos = 0, down = 0, listen = true;
+    $('body').on('DOMMouseScroll mousewheel', function(e) {
         e.preventDefault();
         if(!listen)return;
         listen = false;
         down = e.originalEvent.detail > 0 || e.originalEvent.wheelDelta < 0;
-        c = Math.min(Math.max(0, down ? ++c : --c), tot-1);
-        pagePos = $pages.eq(c).offset().top - 65;
+        current = Math.min(Math.max(0, down ? ++current : --current), tot-1);
+        pagePos = $pages.eq(current).offset().top - 65;
         $(this).stop().animate({scrollTop: pagePos}, 650, function(){
             listen = true;
         });
@@ -16,8 +16,15 @@ $(document).ready(function(){
     $('.navbar-nav a').on('click', function(){
         var eq_value = this.getAttribute('class');
         var dom_to_scroll_to = $('.pages').eq(eq_value).offset().top - 65;
-        c = eq_value;
+        current = eq_value;
         $('body').animate({scrollTop: dom_to_scroll_to}, 650);
-    })
-});
+    });
+    $(window).scroll(function() {
+        if (current == 1) {
+            $('.bar').addClass('animate_bar');
 
+        } else {
+            $('.bar').removeClass('animate_bar');
+        }
+    });
+});
